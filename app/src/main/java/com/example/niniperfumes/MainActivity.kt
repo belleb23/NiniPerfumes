c95a15b6f7eafd106cb058e3f4884e1345a851b0
belleb23 <bellebernieri@gmail.com>
2024-05-18 19:47:07 -0300

    create layout

--- app/src/main/java/com/example/niniperfumes/MainActivity.kt
+++ app/src/main/java/com/example/niniperfumes/MainActivity.kt
@@ -9,9 +9,8 @@ import android.widget.Toast
 class MainActivity : Activity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val view = TextView(this)
-        view.setText("Fantasy 30 ml")
-        setContentView(view)
+        setContentView(R.layout.activity_main)
+
     }
 
 }
