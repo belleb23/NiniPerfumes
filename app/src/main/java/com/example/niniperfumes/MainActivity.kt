db2090ac4baf504ab07b455942c21e8844dd172d
belleb23 <bellebernieri@gmail.com>
2024-05-18 19:28:07 -0300

    first view

--- app/src/main/java/com/example/niniperfumes/MainActivity.kt
+++ app/src/main/java/com/example/niniperfumes/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.niniperfumes
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : Activity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Bem vindo a loja Nini Perfume", Toast.LENGTH_SHORT).show()
+        val view = TextView(this)
+        view.setText("Fantasy 30 ml")
+        setContentView(view)
     }
 
 }
