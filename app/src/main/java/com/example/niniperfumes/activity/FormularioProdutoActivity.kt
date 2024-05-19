6e0efb880489246b81e7cb021bcdf1e7bcbf8706
belleb23 <bellebernieri@gmail.com>
2024-05-19 00:55:06 -0300

    add activity form

---
+++ app/src/main/java/com/example/niniperfumes/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,16 @@
+package com.example.niniperfumes.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.niniperfumes.R
+
+class FormularioProdutoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_produto)
+
+    }
+}
