b98702ccf9ad704b621356d01483952bd9ffd001
belleb23 <bellebernieri@gmail.com>
2024-05-19 15:35:27 -0300

    end binding form

--- app/src/main/java/com/example/niniperfumes/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/example/niniperfumes/activity/FormularioProdutoActivity.kt
@@ -9,16 +9,36 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.niniperfumes.R
+import com.example.niniperfumes.model.Produto
+import java.math.BigDecimal
 
 class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         val botaoSalvar = findViewById<Button>(R.id.botao_salvar)
         botaoSalvar.setOnClickListener{
             val campoNome = findViewById<EditText>(R.id.nome)
             val nome = campoNome.text.toString()
-            Log.i("FormularioProduto", "onCreate: $nome")
+            val campoDescricao = findViewById<EditText>(R.id.descricao)
+            val descricao = campoDescricao.text.toString()
+            val campoValor = findViewById<EditText>(R.id.valor)
+            val valorEmTexto = campoValor.text.toString()
+            val valor = if(valorEmTexto.isBlank()) {
+                BigDecimal.ZERO
+            } else {
+                BigDecimal(valorEmTexto)
+            }
+
+
+            val produtoNovo = Produto(
+                nome = nome,
+                descricao = descricao,
+                valor = valor
+            )
+
+            Log.i("FormularioProduto", "onCreate: $produtoNovo")
         }
     }
 
--- app/src/main/java/com/example/niniperfumes/activity/MainActivity.kt
+++ app/src/main/java/com/example/niniperfumes/activity/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.niniperfumes.activity
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -8,15 +9,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.niniperfumes.R
 import com.example.niniperfumes.model.Produto
 import com.example.niniperfumes.recyclerview.adapter.ListaProdutosAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity(R.layout.activity_main){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)  // Setting the LayoutManager
+
         recyclerView.adapter = ListaProdutosAdapter(this, listOf(
             Produto(nome="Perfume 1", descricao = "Perfume desc", valor = BigDecimal("189.00")),
             Produto(nome="Perfume 2", descricao = "Perfume 2 desc", valor = BigDecimal("19.00")),
@@ -25,6 +26,11 @@ class MainActivity : AppCompatActivity(R.layout.activity_main){
 
             ))
 
+        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        fab.setOnClickListener{
+            val intent = Intent(this, FormularioProdutoActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 
