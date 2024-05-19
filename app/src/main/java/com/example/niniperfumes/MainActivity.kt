673424774b3e6e2fbe45fcd570861e92040b9ff0
belleb23 <bellebernieri@gmail.com>
2024-05-18 23:13:39 -0300

    add recyclerView

--- app/src/main/java/com/example/niniperfumes/MainActivity.kt
+++ app/src/main/java/com/example/niniperfumes/MainActivity.kt
@@ -10,12 +10,6 @@ class MainActivity : Activity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val nome = findViewById<TextView>(R.id.nome)
-        nome.text = "Hipnotic"
-        val descricao = findViewById<TextView>(R.id.descricao)
-        descricao.text = "Doce e marcante"
-        val valor = findViewById<TextView>(R.id.valor)
-        valor.text = "399.00"
 
 
     }
