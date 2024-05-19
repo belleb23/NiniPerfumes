5668850f34b2ce47f6f1e0afb28b409d1732b2c2
belleb23 <bellebernieri@gmail.com>
2024-05-19 15:01:55 -0300

    init binding form

--- app/src/main/java/com/example/niniperfumes/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/example/niniperfumes/activity/FormularioProdutoActivity.kt
@@ -1,6 +1,9 @@
 package com.example.niniperfumes.activity
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,4 +11,15 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.niniperfumes.R
 
 class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val botaoSalvar = findViewById<Button>(R.id.botao_salvar)
+        botaoSalvar.setOnClickListener{
+            val campoNome = findViewById<EditText>(R.id.nome)
+            val nome = campoNome.text.toString()
+            Log.i("FormularioProduto", "onCreate: $nome")
+        }
+    }
+
 }
